bf095a93df1624731532b00f9d8092f130653411
lukamiljus123 <62016060+lukamiljus123@users.noreply.github.com>
2023-04-19 19:06:45 +0200

    initial commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,15 @@
+using System;
+
+[Serializable]
+public struct Block
+{
+    public int id;
+    public string subject;
+    public string grade;
+    public Mastery mastery;
+    public string domainid;
+    public string domain;
+    public string cluster;
+    public string standardid;
+    public string standarddescription;
+}
